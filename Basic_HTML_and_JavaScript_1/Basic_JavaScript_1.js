3574eac38590a93e8cd6abfb409e27334c4e95d9
JeffreyRod <57733139+JeffreyRod@users.noreply.github.com>
2020-07-02 01:17:55 -0700

    Add files via upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+alert("Hello, World! Welcome to my workshop!");
+<script>
+      function MY_My first_Function() {
+	  var String = "Kiss me, I'm Irish!";
+	  var result = String.fontcolor("green")
+	  document.getElementById("Irish").innerHTML =
+	result;
+}
+</script>
